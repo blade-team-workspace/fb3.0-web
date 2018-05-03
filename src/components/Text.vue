01e739137e873e5516a274433fc194406e4beda4
yangpeng <yangpeng@xdf.cn>
2018-05-03 15:27:24 +0800

    text textarea 渲染

---
+++ src/components/Text.vue
@@ -0,0 +1,22 @@
+<template>
+<i-input style="width: 300px" :name="itemData.name">
+
+</i-input>
+</template>
+
+<script>
+export default {
+
+  created() {
+
+  },
+  props : {
+    itemData:{
+      type : Object,
+      required: true
+    }
+
+  }
+}
+
+</script>
---
+++ src/components/TextArea.vue
@@ -0,0 +1,21 @@
+<template>
+  <i-input  type="textarea" style="width: 300px" ></i-input>
+
+</template>
+
+<script>
+  export default {
+
+    created() {
+
+    },
+    props : {
+      itemData:{
+        type : Object,
+        required: true
+      }
+
+    }
+  }
+
+</script>
