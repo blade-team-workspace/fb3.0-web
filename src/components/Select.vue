3c12a27667b1b6f58695cc11d8f8926f8ecd72f6
yangpeng <yangpeng@xdf.cn>
2018-05-03 15:26:48 +0800

    select组件渲染

---
+++ src/components/Select.vue
@@ -0,0 +1,23 @@
+<template>
+  <i-select  style="width:200px">
+    <i-option v-for="item in itemData.options" :value="item.value" :key="item.value">{{ item.label }}</i-option>
+  </i-select>
+</template>
+
+<script>
+  export default {
+
+    created() {
+
+
+    },
+    props : {
+      itemData:{
+        type : Object,
+        required: true
+      }
+
+    }
+  }
+
+</script>
--- src/server/data.js
+++ src/server/data.js
@@ -3,13 +3,41 @@
   "url": "class",
   "type": "form",
   "items": [{
-    "label": "多行-1",
+    "label": "单行文本input",
     "type": "form-group",
     "groupId": "group0",
     "items": [{
       "name": "text1",
-      "type": "textarea"
+      "type": "text"
     }]
+  },{
+    "label": "多行文本textArea",
+    "type": "form-group",
+    "groupId": "group0",
+    "items": [{
+      "name": "textArea1",
+      "type": "textArea"
+    }]
+  },{
+    "label": "下拉单选-1",
+    "type": "form-group",
+    "groupId": "group0",
+    "items": [
+      {
+        "name": "select1",
+        "type": "select",
+        "options": [{
+          "label": "选项1",
+          "value": 1
+        }, {
+          "label": "选项2",
+          "value": 2
+        }, {
+          "label": "选项3",
+          "value": 3
+        }]
+      }
+    ]
   }]
 }]
  export default datalist
